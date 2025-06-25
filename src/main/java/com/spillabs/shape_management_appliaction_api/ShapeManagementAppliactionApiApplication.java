e61ba465c0b5ebcbc0d60cc02b5e03fccef63c27
Dominic Lakshan <dominiclakshan831@gmail.com>
2025-06-25 17:25:23 +0530

    Initialize Spring Boot backend with MySQL and security for shape management app

---
+++ src/main/java/com/spillabs/shape_management_appliaction_api/ShapeManagementAppliactionApiApplication.java
@@ -0,0 +1,13 @@
+package com.spillabs.shape_management_appliaction_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeManagementAppliactionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeManagementAppliactionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spillabs/shape_management_appliaction_api/ShapeManagementAppliactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spillabs.shape_management_appliaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeManagementAppliactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
